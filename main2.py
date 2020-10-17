b43cefc3959c657fe392fd6f522fd314ab0f1e79
AndreyMo1seev <45105942+AndreyMo1seev@users.noreply.github.com>
2020-10-17 21:23:28 +0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,12 +1,14 @@
-def Mass(Peptide, weightOfSubPept):
-    mas = 0
-    for p in Peptide:
-        mas += weightOfSubPept[p]
-    return mas
+def SubPeptide(pept, listOfSubPept):
+    n = len(pept)
+    pept1 = pept
+    pept += pept
+    for i in range(1,len(pept1)):
+        for j in range(0, len(pept1)):
+            listOfSubPept.append(pept[j: j + i])
+    listOfSubPept.append(pept1)
 
 
 def WeightList(listOfSubPept, wList, weightOfSubPept):
-    wList = [0]
     for k in listOfSubPept:
         if len(k) == 1:
             wList.append(weightOfSubPept[k])
@@ -15,72 +17,20 @@ def WeightList(listOfSubPept, wList, weightOfSubPept):
             for i in range(len(k)):
                 tmp += weightOfSubPept[k[i:i + 1]]
             wList.append(tmp)
-    wList.sort()
-    return wList
 
-
-def ParentMass(Spectrum):
-    return max(Spectrum)
-
-
-def Expand(Peptide, weightOfSubPept):
-    oldPeptide = Peptide.copy()
-    Peptide.clear()
-    for i in range(len(oldPeptide)):
-        for k in weightOfSubPept:
-            Peptide.append(oldPeptide[i] + k)
-    return Peptide
-
-
-def SubPeptide(pept): # в презентации эта функция называется Cyclospectrum
-    n = len(pept)
-    listOfSubPept = []
-    pept1 = pept
-    pept += pept
-    for i in range(1, len(pept1)):
-        for j in range(0, len(pept1)):
-            listOfSubPept.append(pept[j: j + i])
-    listOfSubPept.append(pept1)
-    return listOfSubPept
-
-def LinearSubPeptide(pept):
-    n = len(pept)
-    listOfSubPept = []
-    for i in range(1, len(pept)):
-        for j in range(0, len(pept)):
-            if pept[j: j + i] not in listOfSubPept:
-                listOfSubPept.append(pept[j: j + i])
-    listOfSubPept.append(pept)
-    return listOfSubPept
-
-
-def PrintPeptide(Peptide, weightOfSubPept, lst):
-    tmpStr = ""
-    for i in range(len(Peptide) - 1):
-        tmpStr += str(weightOfSubPept[Peptide[i]])
-        tmpStr += "-"
-        # print(weightOfSubPept[Peptide[i]], "-", sep='', end="")
-    tmpStr += str(weightOfSubPept[Peptide[len(Peptide) - 1]])
-    # print(weightOfSubPept[Peptide[len(Peptide) - 1]])
-    if tmpStr not in lst:
-        print(tmpStr)
-        lst.append(tmpStr)
-
-
-def Consistens(Peptide, Spectrum, weightOfSubPept, wList):
-    PeptideSpectr = WeightList(Peptide, wList, weightOfSubPept)
-    tmpPeptSpectr = PeptideSpectr.copy()
-    for s in Spectrum:
-        for m in PeptideSpectr:
-            if m == s:
-                tmpPeptSpectr.remove(m)
-                break
-        PeptideSpectr = tmpPeptSpectr
-    if PeptideSpectr:
-        return False
-    else:
-        return True
-
+def Score(wList, expPeptide):
+    c = 0
+    tmp = expPeptide.copy()
+    lst = []
+    for w in wList:
+        lst = []
+        for e in expPeptide:
+            if w == e and w not in lst:
+                tmp.remove(w)
+                c += 1
+                lst.append(w)
+                expPeptide = tmp
+    return c
 
 weightOfSubPept = {
     'G': 57,
@@ -91,8 +41,10 @@ weightOfSubPept = {
     'T': 101,
     'C': 103,
     'I': 113,
+    'L': 113,
     'N': 114,
     'D': 115,
+    'K': 128,
     'Q': 128,
     'E': 129,
     'M': 131,
@@ -102,34 +54,12 @@ weightOfSubPept = {
     'Y': 163,
     'W': 186
 }
-sSpectrum = input()
-Spectrum = [int(x) for x in sSpectrum.split()]
-Peptide = []
-lst = [] # так как есть пептиды с одинаковыми массами (I & L , K & Q), то мне приходится перед тем как печатать, смотреть не было ли уже такого
-# Для этого добавляю уже напечатанные в lst
-for s in weightOfSubPept:
-    Peptide.append(s)
+peptide = str(input())
+sexpPeptide = str(input())
+expPeptide = [int(x) for x in sexpPeptide.split()]
 listOfSubPept = []
+SubPeptide(peptide, listOfSubPept)
 wList = [0]
-c = 0
-ourPeptide = [] # ourPeptide - массы пептидов, которые присутствуют в нашем спектре
-# for j in Spectrum:
-#     for k in Peptide:
-#         if j == weightOfSubPept[k]:
-#             ourPeptide.append(k)
-# Peptide = ourPeptide.copy()
-while Peptide and c <= 100:
-    tmpPept = Peptide.copy()
-    for p in Peptide:
-        # if p == "TCI":
-            # print(1)
-        if Mass(p, weightOfSubPept) == ParentMass(Spectrum):
-            if WeightList(SubPeptide(p), wList, weightOfSubPept) == Spectrum:
-                PrintPeptide(p, weightOfSubPept, lst)
-                c += 1
-            tmpPept.remove(p)
-        elif not Consistens(LinearSubPeptide(p), Spectrum, weightOfSubPept, wList):
-            tmpPept.remove(p)
-
-    Peptide = tmpPept
-    Peptide = Expand(Peptide, weightOfSubPept)
+WeightList(listOfSubPept, wList, weightOfSubPept)
+print()
+print(Score(wList, expPeptide))
