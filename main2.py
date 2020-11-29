57edb9e5331cfb7f1e2df1f65bae5a60c31be231
AndreyMo1seev <45105942+AndreyMo1seev@users.noreply.github.com>
2020-11-29 16:54:54 +0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,41 +1,74 @@
+def include(a, b):
+    for i in b:
+        if i in a:
+            a.remove(i)
+    if len(a) == 0:
+        return True
+    else:
+        return False
+
+
+def cycle_recover(cycle):
+    result_str = str(cycle[0][0])+'->'+str(cycle[0][1])+'->'
+    for i in range(1, len(cycle) - 1):
+        result_str += str(cycle[i][1])
+        result_str += '->'
+    result_str += str(cycle[len(cycle) - 1][1])
+    return result_str
+
+
+def euler_cycle(connectivity_components):
+    start = connectivity_components[0]
+    cycle = [start]
+    bad_ways = set()
+    counter = 0
+    while True:
+        # for i in range(len(connectivity_components)):
+        for edge in connectivity_components:
+            if start[1] == edge[0] and edge not in cycle and edge not in bad_ways:
+                cycle.append(edge)
+                start = edge
+                bad_ways.clear()
+                break
+        if len(cycle) == len(connectivity_components):
+            break
+
+        for c in reversed(cycle):
+            for ed in connectivity_components:
+                flag = False
+                if c[1] == ed[0] and ed not in cycle and ed not in bad_ways:
+                    start = c
+                    flag = True
+                    break
+            if flag == False:
+                bad_ways.add(c)
+                cycle.pop()
+            else:
+                break
+        if counter == 500:
+            print(101)
+    return cycle
+
+
 # Input ------------------------------------
-s = str(input())
-t = str(input())
-# ------------------------------------------
-matrix = [[0] * (len(t) + 1) for i in range(len(s) + 1)]
-
-
-def LCS_DYN(x, y):
-    L = matrix
-    LCS = []
-    x_i, y_i = len(x) - 1, len(y) - 1
-    while x_i >= 0 and y_i >= 0:
-        if x[x_i] == y[y_i]:
-            LCS.append(x[x_i])
-            x_i, y_i = x_i - 1, y_i - 1
-        elif L[x_i - 1][y_i] > L[x_i][y_i - 1]:
-            x_i -= 1
+def inp():
+    connectivity_components = []
+    while True:
+        line = input()
+        if line:
+            line = line.split('->')
+            str1 = line[1].split(',')
+            for s in str1:
+                connectivity_components.append((int(line[0]), int(s)))
         else:
-            y_i -= 1
-    LCS.reverse()
-    return LCS
+            break
+    return connectivity_components
+# ---------------------------------------------
 
 
-n = len(s)
-m = len(t)
-maxvalue = 0
-c = -1
-r = -1
-result = ''
-for column in range(1, n + 1):
-    for row in range(1, m + 1):
-        matrix[column][0] = matrix[column - 1][0]
-        diag = 0
-        if s[column - 1] == t[row - 1]:
-            diag = 1
-        matrix[column][row] = max(matrix[column - 1][row], matrix[column][row - 1], matrix[column - 1][row - 1] + diag)
-
-
-result = ''.join(LCS_DYN(t, s))
-print(matrix[n][m])
-print(result)
+if __name__ == '__main__':
+    connectivity_components = inp()
+    # connectivity_components = [(0, 3), (1, 12), (1, 2), (10, 1), (11, 10), (12, 11), (13, 3), (14, 13), (15, 14), (2, 0), (2, 4), (3, 1), (3, 15), (4, 6), (4, 7), (5, 2), (6, 5), (7, 9), (8, 4), (9, 8)]
+    cycle = euler_cycle(connectivity_components)
+    result = cycle_recover(cycle)
+    print(result)
