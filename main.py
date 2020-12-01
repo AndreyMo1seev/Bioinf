f6cc1a503c0c100e53cec584f0ac2da4695b56e6
AndreyMo1seev <45105942+AndreyMo1seev@users.noreply.github.com>
2020-12-01 16:11:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,69 +1,47 @@
-def cycle_recover(cycle):  # функция для приведения списка связанности к виду, который требует степик
-    result_str = str(cycle[0][0])+'->'+str(cycle[0][1])+'->'
-    for i in range(1, len(cycle) - 1):
-        result_str += str(cycle[i][1])
-        result_str += '->'
-    result_str += str(cycle[len(cycle) - 1][1])
-    return result_str
+import sys
+from collections import deque
 
 
-def euler_cycle(connectivity_components):  # поиск эйлерова цикла в графе
-    start = connectivity_components[0]
-    cycle = [start]  # стек в который записываются компоненты связанности цикла, который мы ищем
-    bad_ways = set()  #
-    while True:
-        while True:  # ищем какой-то путь, пока не замкнем цикл
-            for edge in connectivity_components:
-                if start[1] == edge[0] and edge not in cycle and edge not in bad_ways:
-                    cycle.append(edge)
-                    start = edge
-                    bad_ways.clear()
-                    break
-            if start[1] == cycle[0][0]:
-                bad_ways.add(start)
-                break
-        if len(cycle) == len(connectivity_components):  # если найденый нами цикл обходит все ребра, то он эйлеров и
-            #  работа функции завершена
-            break
-
-        for c in reversed(cycle):  # проходим по стеку с конца и ищем элемент, из которого мы можем пойти по другому
-            # пути.
-            for ed in connectivity_components:
-                flag = False
-                if c[1] == ed[0] and ed not in cycle and ed not in bad_ways:
-                    start = c
-                    flag = True
-                    break
-            if flag == False:  # если мы не нашли куда можно перейти из текущего ребра в cycle, то мы удаляем это
-                # ребро из cycle и записываем его в bad_ways, чтобы потом снова по нему не пойти
-                bad_ways.add(c)
-                cycle.pop()
-            else:
-                break
-    # эти два цикла чередуют друг друга. Сначала ищем какой-то цикл, и если он не эйлеров, то удаляем элементы из 
-    #     стека пока не дойдем до элемента, из которого можем продолжить цикл по другому пути
-    #  в конечном итоге этот алгоритм должен привести нас к эйлерову пути
+def euler_cycle(connectivity_components, size):  # поиск эйлерова цикла в графе
+    stack = deque()
+    stack.append(0)
+    cycle = []
+    while len(stack) != 0:
+        v = stack.pop()
+        stack.append(v)
+        if v == 0:
+            print(101)
+        if len(connectivity_components[v]) == 0:
+            cycle.append(v)
+            stack.pop()
+        else:
+            stack.append(connectivity_components[v][0])
+            connectivity_components[v].remove(connectivity_components[v][0])
     return cycle
 
 
 # Input ------------------------------------
 def inp():
-    connectivity_components = []
-    while True:
-        line = input()
+    connectivity_components = dict()
+    tmp = []
+    size = 0
+    for line in sys.stdin:
         if line:
             line = line.split('->')
             str1 = line[1].split(',')
+            tmp.clear()
             for s in str1:
-                connectivity_components.append((int(line[0]), int(s)))
+                tmp.append(int(s))
+                size += 1
+            connectivity_components[int(line[0])] = tmp.copy()
         else:
             break
-    return connectivity_components
+    return connectivity_components, size
 # ---------------------------------------------
 
 
 if __name__ == '__main__':
-    connectivity_components = inp()  # список связанности 
-    cycle = euler_cycle(connectivity_components)
-    result = cycle_recover(cycle)
-    print(result)
+    connectivity_components, size = inp()  # список связанности
+    cycle = euler_cycle(connectivity_components, size)
+    cycle.reverse()
+    print("->".join(map(str, cycle)))
